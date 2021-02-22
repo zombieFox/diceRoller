81755621c5e89c8f12c1d30caeff9cbc23b48221
zombieFox <crudetoaster@gmail.com>
2021-02-22 16:36:09 +0000

    remove unused modules

--- src/component/base/index.js
+++ src/component/base/index.js
@@ -1,13 +1,10 @@
 import './reset.css';
 import './font.css';
 import './base.css';
-import './tag.css';
 import './state.css';
 import './animation.css';
 import './typography.css';
 import './form.css';
-import './spacing.css';
-import './zIndex.css';
 import './utilities.css';
 
 export const base = {};
